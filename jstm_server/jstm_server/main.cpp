3804c7643700f50d5bfe000cea0ab6b1705b426c
jysso3070 <48274155+jysso3070@users.noreply.github.com>
2020-07-28 18:03:01 +0900

    Update main.cpp

--- jstm_server/jstm_server/main.cpp
+++ jstm_server/jstm_server/main.cpp
@@ -1,14 +1,14 @@
 #include "header.h"
 #include "iocp_server.h"
 
+
 int main() {
 
 	cout << sizeof(DirectX::XMFLOAT4X4) << endl;;
 	cout << sizeof(unsigned short) << endl;;
 	cout << sizeof(sc_packet_monster_pos) << endl;;
 
-	
-	Iocp_server g_iocp_server;
+	Iocp_server *g_iocp_server = new Iocp_server;
 
 }
 
